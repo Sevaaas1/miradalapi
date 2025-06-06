42c75468a08e454a3b6fce3a33a18be0e1ca163a
Bebelin <63244054+Sevaaas1@users.noreply.github.com>
2025-06-05 23:13:21 -0400

    correcion GET

--- src/app/route.ts
+++ src/app/route.ts
@@ -1,5 +1,20 @@
 import { NextResponse } from "next/server";
-
+import {v4 as uuidv4} from 'uuid';
+let datos = [
+  {
+    id:uuidv4(),
+    respuestas:{
+      item:["si","no","perro"]
+    }
+  }
+]
 export async function GET() {
-  return NextResponse.json({ message: "Hello world!" });
+  return NextResponse.json( datos );
+}
+
+export async function POST() {
+
+    const nuevoUsuario = { id: uuidv4(), respuestas:{item:["no","ne","na"]} };
+    datos.push(nuevoUsuario);
+    return NextResponse.json(nuevoUsuario);
 }
